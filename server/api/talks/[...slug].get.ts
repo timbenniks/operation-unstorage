9e786d695f4bb60704ddc85f4fdabbb369c3782f
Tim Benniks <tbenniks@gmail.com>
2024-07-06 18:21:14 +0200

    file based for performance

--- server/api/talks/[...slug].get.ts
+++ server/api/talks/[...slug].get.ts
@@ -1,5 +1,5 @@
 import { eventHandler } from 'h3'
-import { ofetch } from "ofetch";
+import { talks } from '../../data/talks.json';
 
 type MarkdownData = {
   [key: string]: string;
@@ -15,91 +15,41 @@ type Talk = {
   location: string;
 }
 
-async function getTalkKeys(endpoint: string) {
-  const { data } = await ofetch(endpoint, {
-    method: "post",
-    body: {
-      query: `
-          query Talks($first: Int!) {
-            talks(first: $first, orderBy: date_DESC) {
-              id
-            }
-          }
-          `,
-      variables: {
-        first: 200,
-      },
-    },
-  });
-
-  return data.talks
-};
-
-async function getTalk(endpoint: string, id: string) {
-  const { data } = await ofetch(endpoint, {
-    method: "post",
-    body: {
-      query: `
-          query Talk($id: ID!) {
-            talk(where: { id: $id }) {
-              id
-              conference
-              talk
-              location
-              date
-              link
-            }
-          }
-          `,
-      variables: {
-        id,
-      },
-    },
-  });
-
-  return data.talk
-};
-
-
 function convertToMarkdown(markdownData: MarkdownData) {
   const frontMatter = Object.entries(markdownData)
     .filter(([key]) => key !== "body")
     .map(([key, value]) => `${key}: ${JSON.stringify(value)}`)
     .join("\n");
 
-  const markdownContent = `---\n${frontMatter}\n---\n\n${markdownData.body ? markdownData.body : ''}`;
-
-  return markdownContent;
+  return `---\n${frontMatter}\n---\n\n${markdownData.body ? markdownData.body : ''}`;
 }
 
 export default eventHandler(async (event) => {
-  const runtimeConfig = useRuntimeConfig()
-  const endoint = runtimeConfig.gqlEndpoint
-
   const slug = event.context.params?.slug
 
-
   // unstorage getKeys
-  if (slug === ':') {
-    const talks = await getTalkKeys(endoint)
+  if (slug && slug === ':') {
     return talks.map((talk: any) => {
       return `${talk.id}.md`
     })
   }
 
   // unstorage getMeta
-  if (slug?.endsWith('$')) {
+  if (slug && slug?.endsWith('$')) {
     return {
-      "meta": "value"
+      "meta": "YOLO"
     }
   }
 
   // unstorage getItem
-  const talkInfo = await getTalk(endoint, slug?.replace(".md", "") as string)
+  const id = slug?.replace(".md", "") as string
+  const talk = talks.find((talk: Talk) => talk.id === id)
+
+  if (!talk) {
+    return false
+  }
+
   return convertToMarkdown({
-    "title": talkInfo.talk,
-    "description": talkInfo.conference,
-    ...talkInfo,
-    // "body": `# ${talkInfo.talk} on ${talkInfo.conference} at ${talkInfo.date}`
+    ...talk
   });
 })
